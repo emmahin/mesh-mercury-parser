07cc7622151e2216cda7db0ecd611b3ecb8a2f78
emmahin <emma@mesh-impact.com>
2025-05-29 15:53:54 +0200

    Update index.js
    added const striptags = require('striptags');
    
    in order to clean the content from all html content.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Mercury = require('@postlight/parser');
+const striptags = require('striptags');
 
 const app = express();
 app.use(bodyParser.json());
